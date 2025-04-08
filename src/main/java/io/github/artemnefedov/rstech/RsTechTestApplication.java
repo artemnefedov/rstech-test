2b955715f7aea0601f267b401f2c21419d652107
Artem Nefedov <artemnefedov@duck.com>
2025-04-08 17:35:36 +0400

    init repo

---
+++ src/main/java/io/github/artemnefedov/rstech/RsTechTestApplication.java
@@ -0,0 +1,12 @@
+package io.github.artemnefedov.rstech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsTechTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsTechTestApplication.class, args);
+	}
+}
---
+++ src/main/java/io/github/artemnefedov/rstech/dto/Product.java
@@ -0,0 +1,9 @@
+package io.github.artemnefedov.rstech.dto;
+
+public record Product(
+        String name,
+        String description,
+        float price
+) {
+
+}
